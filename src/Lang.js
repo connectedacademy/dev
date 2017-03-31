3e0f798739632e81b15f77d39643d3fc6445043c
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-31 10:51:18 +0100

    Resolved loading issue caused by missing translation file

--- src/Lang.js
+++ src/Lang.js
@@ -3,9 +3,7 @@ import Vuex from 'vuex';
 import VueI18n from 'vue-i18n';
 
 import langEn from './lang/en';
-import langEs from './lang/es';
 
 Vue.use(VueI18n);
 
 Vue.locale('en', langEn);
-Vue.locale('es', langEs);
