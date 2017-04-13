42843e19e60c840dc93e425dd07c50969d270c15
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-04-13 14:57:12 +0100

    Set profile image

--- src/components/navigation/ProfileIcon.vue
+++ src/components/navigation/ProfileIcon.vue
@@ -1,40 +1,48 @@
 <template lang="pug">
 
+
 	.profile-icon.animated.bounceIn
-		.profile-image
+		.profile-image(v-bind:style="{ 'background-image': profile }")
+
 
 </template>
 
 <script>
-
-	export default {
-	  name: 'profile-icon',
-	  data() {
-	    return {
-	      stateClose: false,
-	    };
-	  },
-	};
-
+export default {
+  name: 'profile-icon',
+  data() {
+    return {
+      stateClose: false,
+    };
+  },
+  computed: {
+    profile() {
+      return `url('${this.$store.state.auth.user.profile}')`;
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
 
 @import "../../assets/stylus/shared/*"
 
-	.profile-icon
-		position absolute
-		top 0
-		right 0
-		padding 10px
-		transition right 0.6s
-		.profile-image
-			background-color white
-			border-radius 50%
-			height 40px
-			width 40px
+.profile-icon
+  position absolute
+  top 0
+  right 0
+  padding 10px
+  transition right 0.6s
+  .profile-image
+    background-color white
+    background-size cover
+    background-repeat no-repeat
+    background-position center
+    border-radius 50%
+    height 40px
+    width 40px
 
-		&:hover
-			cursor pointer
+    &:hover
+      cursor pointer
 
 </style>
