29f085c46f48cea3c7f31236c6f3b7f7161b81a0
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-10-24 18:03:50 +0100

    Updates

--- src/components/MediaContainer.vue
+++ src/components/MediaContainer.vue
@@ -9,7 +9,7 @@
       .pure-button(@click="reslick") reslick
       //- img#current-image(v-bind:src="`https://${course.slug}.connectedacademy.io/course/content/media/small/${media[currentIndex].text}`" @click="setLightboxMedia(media[currentIndex].text)")
       //- img#next-image(v-if="!slickMode && nextIndex" v-bind:src="`https://${course.slug}.connectedacademy.io/course/content/media/small/${media[nextIndex].text}`" @click="setLightboxMedia(media[nextIndex].text)")
-      slick#image-swiper(v-if="slickMode" ref="classslick" v-bind:options="slickOptions")
+      slick#image-swiper(v-if="slickMode && media" ref="classslick" v-bind:options="slickOptions")
         .img-wrapper(v-for="(item, index) in media" v-bind:key="index" )
           img(v-bind:data-lazy="`https://researchmethods.connectedacademy.io/course/content/media/small/${item.text}`" @click="setLightboxMedia(item.text)")
           //- img(v-bind:data-lazy="`https://${course.slug}.connectedacademy.io/course/content/media/small/${item.text}`" @click="setLightboxMedia(item.text)")
