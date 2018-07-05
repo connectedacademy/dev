b9656c42255ba60b4a2ddcb2ff4e072945c2457d
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-07-05 21:29:22 +0000

    Updates

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   name: 'Connected Academy',
   version: '0.0.3',
-  built: '2018-7-5 20:55:45',
+  built: '2018-7-5 21:23:27',
   segmentHeight: 158,
   ga_id: 'UA-44963053-16',
   sentry: 'https://cd5136ba6a3b46a79ade2112cb23d036@sentry.io/176250'
