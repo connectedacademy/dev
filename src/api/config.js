fe2a4daf8795424a279ea472eb9837d357c50ecb
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-08-22 14:42:51 +0100

    Resolved typo

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
 export const API = process.env.API_ROUTE
-export const TWITTER_TWITTER_AUTH_URL = `${process.env.API_ROUTE}/auth/twitter/login`
+export const TWITTER_AUTH_URL = `${process.env.API_ROUTE}/auth/twitter/login`
