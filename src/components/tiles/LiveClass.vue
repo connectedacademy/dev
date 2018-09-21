125a6aa4e4e34502dacc09f8591b67426e0e40c4
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-09-21 14:30:48 +0100

    Added scheduled classes to liveclass tile

--- src/components/tiles/LiveClass.vue
+++ src/components/tiles/LiveClass.vue
@@ -1,18 +1,25 @@
 <template lang="pug">
-.content-item.liveclass-item(v-if="$route.name === 'content'" v-bind:class="{ independent: independent }")
+.content-item.liveclass-item(v-if="$route.name === 'content'" :class="{ independent: independent }")
   .content
     h1
       icon(:icon="{ prefix: 'fab', iconName: 'twitter' }" v-if="course.engine === 'twitter'" title="Messages are published on Twitter")
       icon(icon="comment" v-if="course.engine === 'local'" title="Messages are stored on Connected Academy")
       | {{ content.title }}
     p {{ independent ? 'This class has a live twitter based discussion, click below to join it.' : content.description }}
-  router-link.pure-button.pure-button-transparent.no-margin(v-bind:to="{ name: 'live' }")
+  .scheduled-classes(v-if="content.classes")
+    .info-tile(v-for="theClass in content.classes")
+      .inner-wrapper
+        .info-title {{ theClass.location }}
+        .info-subtitle {{ formatDate(theClass.date) }}
+
+  router-link.pure-button.pure-button-transparent.no-margin(:to="{ name: 'live' }")
     | Join Class
   .clearfix
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import moment from 'moment-mini'
 
 export default {
   name: 'live-class',
@@ -21,12 +28,18 @@ export default {
     ...mapGetters([
       'course'
     ])
+  },
+  methods: {
+    formatDate(date) {
+      return moment(date).format('HH:mm on dddd do MMMM')
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 
+@import '~stylus/buttons'
 @import '~stylus/action-tile'
 
 .content-item
@@ -37,4 +50,29 @@ export default {
       box-sizing()
       max-width none
       width 100%
+
+.scheduled-classes
+  display flex
+  flex-wrap wrap
+  margin 0 -10px 10px -10px
+  .info-tile
+    box-sizing border-box
+    display block
+    flex-basis 50%
+    padding 10px
+    @media(max-width: 760px)
+      flex-basis 100%
+    .inner-wrapper
+      radius(10px)
+      border alpha(white, 0.3) 1px solid
+      padding 15px 20px
+      .info-title
+        color white
+        font-weight bold
+      .info-subtitle
+        color white
+      .info-action
+        color white
+        margin 0
+        margin-top 10px
 </style>
