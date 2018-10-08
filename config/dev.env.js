4250d03c8c2e56403205ec2de9ea151e633cdfa0
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-10-08 11:08:06 +0100

    Updated local route

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROUTE: `"${process.env.API_ROUTE || "http://ca.local:3000/v1"}"`
-})
+  API_ROUTE: `"${process.env.API_ROUTE || 'http://localhost:3000/v1'}"`,
+});
